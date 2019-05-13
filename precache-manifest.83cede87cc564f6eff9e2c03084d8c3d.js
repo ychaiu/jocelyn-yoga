c76529175e48bd9db3f186e853de5ada101b6fe1
Yessenia <ychaiu@gmail.com>
2019-05-12 21:23:19 -0700

    Updates

---
+++ precache-manifest.83cede87cc564f6eff9e2c03084d8c3d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "097736803e212a8e27acd25b8b1e953b",
+    "url": "/jocelyn-yoga/static/media/DSC04275.09773680.jpg"
+  },
+  {
+    "revision": "02e4e20aa0c6e546bd2542b4bcb717c0",
+    "url": "/jocelyn-yoga/static/media/DSC03467_ 2.02e4e20a.jpg"
+  },
+  {
+    "revision": "48821d7a232ccff16617a6b9d766f983",
+    "url": "/jocelyn-yoga/static/media/DSC03419_ 2.48821d7a.jpg"
+  },
+  {
+    "revision": "3c16e78af24d8d2cba373c5ad743f260",
+    "url": "/jocelyn-yoga/static/media/DSC03415 2.3c16e78a.jpg"
+  },
+  {
+    "revision": "09d510276492dea4bce5",
+    "url": "/jocelyn-yoga/static/js/runtime~main.7f39f78c.js"
+  },
+  {
+    "revision": "f098a5789e5c7cf1f7e4",
+    "url": "/jocelyn-yoga/static/js/main.f2a78ee4.chunk.js"
+  },
+  {
+    "revision": "7dc2786f1315f26fd680",
+    "url": "/jocelyn-yoga/static/js/2.482009be.chunk.js"
+  },
+  {
+    "revision": "f098a5789e5c7cf1f7e4",
+    "url": "/jocelyn-yoga/static/css/main.f7552e1a.chunk.css"
+  },
+  {
+    "revision": "b132f0c031746ede03f5ad169df1ddda",
+    "url": "/jocelyn-yoga/index.html"
+  }
+];
